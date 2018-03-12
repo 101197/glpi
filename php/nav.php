887b16d2f262f25b699ef0882fc52b3ec399a0c2
PaulinePro <paulinedelhaye6298@gmail.com>
2018-03-12 13:38:30 +0100

    jyfuuf

--- php/nav.php
+++ php/nav.php
@@ -6,8 +6,8 @@
   <link rel="stylesheet" href="../css/nav.css"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <script src="js/vendor/jquery-3.3.1.min.js"></script>
-  <script src="js/vendor/bootstrap.min.js"></script>
+  <script src="../js/vendor/jquery-3.3.1.min.js"></script>
+  <script src="../js/vendor/bootstrap.min.js"></script>
 </head>
 <body>
 
