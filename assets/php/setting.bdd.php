49b62ab126c42c1b06d058dd8301938b6fa41f57
PaulinePro <paulinedelhaye6298@gmail.com>
2018-03-19 14:50:58 +0100

    change bdd

--- assets/php/setting.bdd.php
+++ assets/php/setting.bdd.php
@@ -3,7 +3,7 @@ session_start();
 
 //connexion à la bdd
 try {
-  $bdd = new PDO('mysql:host=ppe2.ddns.net;dbname=glpi', 'glpoulpi', 'glpoulpi');
+  $bdd = new PDO('mysql:host=ppe2.ddns.net;dbname=glpoulpi', 'glpoulpi', 'glpoulpi');
 } catch (Exception $e) {
   echo 'Erreur de connexion à la bdd : '.$e;
 }
